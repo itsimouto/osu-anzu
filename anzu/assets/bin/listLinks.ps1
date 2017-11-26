11eab4d7b35a25a6474b37a4318fe92edf262c17
annie <>
2017-11-26 03:45:28 -0500

    .

--- anzu/assets/bin/listLinks.ps1
+++ anzu/assets/bin/listLinks.ps1
@@ -1,4 +1,4 @@
-# Run powershell -ExecutionPolicy Bypass , then call this script.
+# Run this script through listLinks.bat.
 # Dumps a listLinks.txt with all file/directory links.
 # Issue: Also lists target files as HardLinks when they should be excluded. Needs to be fixed.
 
